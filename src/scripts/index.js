dcd4ba14d6c77045eb05a27089b3d0cd0f998abf
Luke Horvat <lukehorvat@gmail.com>
2017-10-03 11:28:27 +1100

    Enable analytics

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -88,3 +88,11 @@ function render() {
   // Render the scene!
   renderer.render(scene, camera);
 }
+
+// Google Analytics.
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+ga("create", "UA-24505142-7", "auto");
+ga("send", "pageview");
