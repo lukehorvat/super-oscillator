6e3e3133cb92c869116134c2c8368bfb9cff943b
Luke Horvat <lukehorvat@gmail.com>
2024-02-15 22:25:12 +0100

    Create welcome screen programmatically

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import * as welcomeScreen from './lib/welcome-screen';
+import * as welcomeScreen from './lib/welcome-screen';
 // import * as loadingScreen from './lib/loading-screen';
 // import { SceneManager } from './lib/scene-manager';
 import './index.css';
@@ -7,7 +7,7 @@ void main();
 
 async function main(): Promise<void> {
   const appEl = document.querySelector('.app')!;
-  // await welcomeScreen.render(appEl);
+  await welcomeScreen.render(appEl);
   // const assetCache = await loadingScreen.render(appEl);
   // const sceneManager = new SceneManager(assetCache);
   // sceneManager.render(appEl);
